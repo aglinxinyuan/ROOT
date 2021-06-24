6fe35b304678edf9a08b4c81472fcd55be1f50fd
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-06-24 14:17:11 -0500

    maps
    1

--- WebContent/map.js
+++ WebContent/map.js
@@ -1,10 +1,7 @@
 
-// function initMap(){
-//     let options ={
-//         zoom:8,
-//         center:{lat:41.8781, lng: 87.6298}
-//     }
-//
-//     let map = new google.maps.Map(document.getElementById('map'),options);
-//
-// }
+function initMap(){
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
