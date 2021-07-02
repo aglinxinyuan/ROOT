468a105c1533c22645fa8fb4f8e027d44cd71fa0
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-07-01 21:16:00 -0500

    1
    1

--- WebContent/create.js
+++ WebContent/create.js
@@ -16,63 +16,83 @@
 // })
 
 $(function(){
-    $("#fileupload").change(function(event) {
-        let x = URL.createObjectURL(event.target.files[0]);
-        $("#upload-img").attr("src",x);
+    $("#fileupload1").change(function(event) {
+        let var1 = URL.createObjectURL(event.target.files[0]);
+        $("#upload-img1").attr("src",var1);
+        console.log(event);
+    });
+
+    $("#fileupload2").change(function(event) {
+        let var2 = URL.createObjectURL(event.target.files[0]);
+        $("#upload-img2").attr("src",var2);
+        console.log(event);
+    });
+
+    $("#fileupload3").change(function(event) {
+        let var3 = URL.createObjectURL(event.target.files[0]);
+        $("#upload-img3").attr("src",var3);
+        console.log(event);
+    });
+
+    $("#fileupload4").change(function(event) {
+        let var4 = URL.createObjectURL(event.target.files[0]);
+        $("#upload-img4").attr("src",var4);
         console.log(event);
     });
 })
-//
-// $("#jquery-upload-button").click(function(){
-//     let formData = new FormData();
-//     let file = $("#file")[0].files[0];
-//     formData.append('file', file);
-//
-//     $.ajax({
-//         url:'upload.php',
-//         type:'post',
-//         data: formData,
-//         contentType: false,
-//         processData: false,
-//         success:function(data){
-//                 if (data !== 0){
-//                 alert('Successful JQuery file upload to: '+ data);
-//             }
-//             else{
-//                 alert('JQuery file upload error.');
-//             }
-//         },
-//     });
-// });
-//
-$(document).ready(function(){
 
-    $("#but_upload").click(function(){
 
-        let fd = new FormData();
-        let files = $('#file')[0].files;
+$("#jquery-upload-button").click(function(){
+    let formData = new FormData();
+    let file = $("#file")[0].files[0];
+    formData.append('file', file);
 
-        // Check file selected or not
-        if(files.length > 0 ){
-            fd.append('file',files[0]);
-
-            $.ajax({
-                url: 'upload.php',
-                type: 'post',
-                data: fd,
-                contentType: false,
-                processData: false,
-                success: function(response){
-                    if(response !== 0){
-                        $("#img").attr("src",response);
-                        $(".preview img").show(); // Display image element
-                    }else{
-                        alert('file not uploaded');
-                    }
-                },
-            });
-        }else{
-            alert("Please select a file.");
-        }
+    $.ajax({
+        url:'upload.php',
+        type:'post',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success:function(data){
+                if (data !== 0){
+                alert('Successful JQuery file upload to: '+ data);
+            }
+            else{
+                alert('JQuery file upload error.');
+            }
+        },
     });
 });
+
+//
+// $(document).ready(function(){
+//
+//     $("#but_upload").click(function(){
+//
+//         let fd = new FormData();
+//         let files = $('#file')[0].files;
+//
+//         // Check file selected or not
+//         if(files.length > 0 ){
+//             fd.append('file',files[0]);
+//
+//             $.ajax({
+//                 url: 'upload.php',
+//                 type: 'post',
+//                 data: fd,
+//                 contentType: false,
+//                 processData: false,
+//                 success: function(response){
+//                     if(response !== 0){
+//                         $("#img").attr("src",response);
+//                         $(".preview img").show(); // Display image element
+//                     }else{
+//                         alert('file not uploaded');
+//                     }
+//                 },
+//             });
+//         }else{
+//             alert("Please select a file.");
+//         }
+//     });
+// });
