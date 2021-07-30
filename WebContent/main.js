28751c4f59f1bf50bb08ab7056125d208f4e3509
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-07-30 10:48:18 -0500

    Location update
    location function

--- WebContent/main.js
+++ WebContent/main.js
@@ -11,3 +11,31 @@ jQuery.ajax({
     url: "activity"+window.location.search, // Setting request url, which is mapped by StarsServlet in Stars.java
     success: (resultData) => handleMainResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
 });
+
+
+//
+// const findMyState = ()=> {
+//     const status = document.querySelector('.status');
+//
+//     const success = (position) =>{
+//         console.log(position)
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+//
+//         const geoApiUrl = 'http://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}localityLanguage=en'
+//
+//         fetch(geoApiUrl)
+//             .then(res => res.json())
+//             .then(data => {
+//                 status.textContent = data.city
+//             })
+//     }
+//
+//     const err = (position) =>{
+//         status.textContent = 'Unable to retrieve your location'
+//     }
+//
+//     navigator.geolocation.getCurrentPosition(success, err);
+//
+// }
+// document.querySelector('#find-state').addEventListener('click', findMyState);
