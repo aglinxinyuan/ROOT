4c6153fc7d70f7db124e44c96e71a73e6956942a
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-08-06 14:28:12 -0500

    create
    create

--- WebContent/create.js
+++ WebContent/create.js
@@ -27,6 +27,26 @@ function nextPrev(n) {
 
 
 
+function validateForm() {
+    // This function deals with validation of the form fields
+    let x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value === "") {
+            // add an "invalid" class to the field:
+            y[i].className += " invalid";
+            // and set the current valid status to false
+            valid = false;
+        }
+    }
+
+    return valid; // return the valid status
+}
+
+
 
 
 
