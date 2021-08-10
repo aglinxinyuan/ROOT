0f140a9d4749c4568430327898666459aa31c42f
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-08-10 15:53:34 -0500

    1
    1

--- WebContent/main.js
+++ WebContent/main.js
@@ -2,7 +2,7 @@ function handleMainResult(resultData) {
     let element = $("#events");
     for (let i = 0; i < resultData.length; i++) {
         let rowHTML = '<div class="block">' +
-            '<a href="http://google.com"><img  src="img/gym.png" height = 160 alt=""></a>' +
+            '<a href="activity.html"><img  src="img/gym.png" height = 160 alt=""></a>' +
             '<div class="eventTitle pl-4"><div id="title" class="semiSC_7">' + resultData[i]["title"] + '</div></div>' +
             '<div class="eventLocation pl-4"><div id="location" class="semiSC_8"><img class="mr-1" src="img/location.png" height=10 alt="">' + resultData[i]["location"] + '</div></div>' ;
         element.append(rowHTML);
@@ -17,29 +17,30 @@ $.ajax({
     success: (resultData) => handleMainResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
 });
 
-//
-// const findMyState = ()=> {
-//     const status = document.querySelector('.status');
-//
-//     const success = (position) =>{
-//         console.log(position)
-//         const latitude = position.coords.latitude;
-//         const longitude = position.coords.longitude;
-//
-//         const geoApiUrl = 'http://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}localityLanguage=en'
-//
-//         fetch(geoApiUrl)
-//             .then(res => res.json())
-//             .then(data => {
-//                 status.textContent = data.city
-//             })
-//     }
-//
-//     const err = (position) =>{
-//         status.textContent = 'Unable to retrieve your location'
-//     }
-//
-//     navigator.geolocation.getCurrentPosition(success, err);
-//
-// }
-// document.querySelector('#find-state').addEventListener('click', findMyState);
+
+
+const findMyState = ()=> {
+    const status = document.querySelector('.status');
+
+    const success = (position) =>{
+        console.log(position)
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const geoApiUrl = 'http://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}localityLanguage=en'
+
+        fetch(geoApiUrl)
+            .then(res => res.json())
+            .then(data => {
+                status.textContent = data.city
+            })
+    }
+
+    const err = (position) =>{
+        status.textContent = 'Unable to retrieve your location'
+    }
+
+    navigator.geolocation.getCurrentPosition(success, err);
+
+}
+document.querySelector('#find-state').addEventListener('click', findMyState);
