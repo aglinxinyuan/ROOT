35aca20eb2c016d4b3106163298af319757bb3d0
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-08-19 16:21:59 -0500

    map
    map

--- WebContent/map.js
+++ WebContent/map.js
@@ -1,7 +1,110 @@
 let map;
-function initMap(){
+
+
+let places = ["130 Sims Dr Syracuse, NY",
+    "130 Sims Dr Syracuse, NY",
+    "222 Waverly Ave Syracuse, NY 13244",
+    "Eggers Hall 200 Syracuse, NY 13244",
+    "Syracuse University, Address"];
+
+
+
+function initMap() {
+
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        center: new google.maps.LatLng(43.039235545230916, -76.13521875124395),
+        zoom: 16,
     });
+
+    const img = new Image();
+    img.src = './img/orangewhitecircle.png';
+
+
+    const icons = {
+        activity: {
+            icon: img.src,
+        },
+        // library: {
+        //     icon: iconBase + "library_maps.png",
+        // },
+        // info: {
+        //     icon: iconBase + "info-i_maps.png",
+        // },
+    };
+
+
+    function geocode(loc){
+        let lat,lng;
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+            params:{
+                address: loc,
+                key: 'AIzaSyCRgoiGsS5sT-jnQ1DbD61LI_1AdoynHIg'
+            }
+        })
+            .then( function subFunction(response){
+                // Log full response in the console
+                console.log(response);
+
+                // Formatted Address
+                let formattedAddress = (response.data.results[0].formatted_address);
+                let formattedAddressOutput = `
+                <ul class = "list-group">
+                <li class ="list-group-item">${formattedAddress}</li>
+                </ul>
+            `;
+
+                // Address Components
+                // let addressComponents = response.data.results[0].address_components;
+                // let addressComponentsOutput = '<ul class = "list-group">';
+                // for(let i =0; i < addressComponents.length; i++){
+                //     addressComponentsOutput += `
+                //     <li class="list-group-item"><srong>${addressComponents[i].types[0]}</srong>: ${addressComponents[i].long_name}</li>
+                // `;
+                // }
+                // addressComponentsOutput += '</u>'
+
+                // Geometry
+                lat= (response.data.results[0].geometry.location.lat);
+                lng= (response.data.results[0].geometry.location.lng);
+                // console.log("returned value:"+lat);
+                // console.log("returned value:"+lng);
+
+                const marker = new google.maps.Marker({
+                    position: response.data.results[0].geometry.location,
+                    icon: icons["activity"].icon,
+                    map: map,
+                });
+
+                new MarkerClusterer(map, markers, {
+                    imagePath:
+                        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+                });
+
+
+            //     let geometryOutput = `
+            //     <ul class = "list-group">
+            //     <li class ="list-group-item"><strong>Latitude</strong>:${lat}</li>
+            //     <li class ="list-group-item"><strong>Longitude</strong>:${lng}</li>
+            //     </ul>
+            // `;
+
+
+                //output to app
+                // document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
+                // document.getElementById('address-components').innerHTML = addressComponentsOutput;
+                // document.getElementById('geometry').innerHTML = geometryOutput;
+
+                // console.log("lat's value: " + response.data.results[0].geometry.location.lat);
+
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+
+    for (let i = 0; i < places.length; i++){
+        geocode(places[i]);
+    }
 }
+
+
