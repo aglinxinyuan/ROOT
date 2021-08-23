06627b6bfa7ee6bd540252ad5501fcfd7768d6a6
Xinyuan Lin <xinyual3@uci.edu>
2021-08-23 07:13:19 -0700

    Update congratulation.js

--- WebContent/signup/congratulation.js
+++ WebContent/signup/congratulation.js
@@ -1,1 +1,6 @@
-$("#email").html(window.location.search.substring(1));
+$.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/verification?id="+atob(window.location.search.substring(1)), // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleMainResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
