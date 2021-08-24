4ac1dce1824c7077af7d6652415b3d7e46ce0863
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-08-24 11:38:35 -0500

    1
    1

--- WebContent/profile.js
+++ WebContent/profile.js
@@ -2,10 +2,12 @@ function handleProfileResult(resultData) {
     console.log("something here");
     // $("#name").text(resultData[15]['title'])
     // $("#aboutme").text(resultData[15]['location'])
-    $("#school").text(resultData[0]['school'])
-    $("#major").text(resultData[0]['major'])
+    $("#name").text(resultData[1]['name'])
+    $("#email").text(resultData[1]['email'])
+    $("#school").text(resultData[1]['school'])
+    $("#major").text(resultData[1]['major'])
     console.log("something here");
-    console.log(resultData['user']);
+    console.log(resultData);
 }
 
 $.ajax({
