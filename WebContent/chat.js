a10662241ee99275eb8538cc08f7cd72d936c61e
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-08-26 16:50:26 -0500

    1
    1

---
+++ WebContent/chat.js
@@ -0,0 +1,31 @@
+// function handleMessageResult(resultData) {
+//
+//     document.getElementById("name").value = resultData[0]['name'];
+//     document.getElementById("gender").value = resultData[0]['gender'];
+//     document.getElementById("school").value = resultData[0]['school'];
+//     document.getElementById("major").value = resultData[0]['major'];
+//     document.getElementById("birth").value = resultData[0]['birth'];
+//     document.getElementById("email").value = resultData[0]['email'];
+//     document.getElementById("phone").value = resultData[0]['phone'];
+//     document.getElementById("aboutme").value = resultData[0]['aboutme'];
+// }
+//
+// $.ajax({
+//     dataType: "json", // Setting return data type
+//     method: "GET", // Setting request method
+//     url: "api/message", // Setting request url, which is mapped by StarsServlet in Stars.java
+//     success: (resultData) => handleMessageResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+// });
+
+
+function submitMessage_form(){
+
+    $.ajax(
+        "api/editProfile", {
+            method: "POST",
+            data: $("#message_form").serialize(),
+            success: window.location.replace("chat.html")
+        }
+    );
+
+}
---
+++ src/SendMessage.java
@@ -0,0 +1,53 @@
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+
+
+
+@WebServlet(name = "SendMessage", urlPatterns = "/api/message")
+public class SendMessage extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/ez-cross");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        String message = request.getParameter("message");
+
+        try (Connection conn = dataSource.getConnection()) {
+            User user = (User) request.getSession().getAttribute("user");
+            String query = "INSERT INTO event(user_id, message) VALUES( ?, ?);";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, user.GetId());
+            statement.setString(2, message);
+
+
+            System.out.println(statement);
+
+
+            statement.executeUpdate();
+            statement.close();
+            response.setStatus(200);
+        } catch (Exception e) {
+            response.getWriter().write(e.getMessage());
+            response.setStatus(500);
+        }
+    }
+}
