b5873fc2600bf056b7105d285f60a5cd94fe9ace
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-08-26 19:46:10 -0500

    1
    1

--- WebContent/chat.js
+++ WebContent/chat.js
@@ -18,6 +18,29 @@
 // });
 
 
+function handleChatResult(resultData) {
+    let element = $("#messages")
+
+    for (let i = resultData.length-1; i >=0 ; i--) {
+        let rowHTML="";
+        rowHTML +=
+            '<li><div class="block mb-3">' +
+            '<a href="activity.html?id='+resultData[i]["id"]+'"><img  src="img/gym.png" height = 160 alt=""></a>' +
+            '<div class="eventTitle pl-4"><div id="title" class="semiSC_7">' + resultData[i]["title"] + '</div></div>' +
+            '<div class="eventLocation pl-4"><div id="location" class="semiSC_8"><img class="mr-1" src="img/location.png" height=10 alt="">' + resultData[i]["location"] + '</div></div></li>' ;
+        element.append(rowHTML);
+    }
+}
+
+$.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/chatroom", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleChatResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
+
+
+
 function submitMessage_form(){
 
 
