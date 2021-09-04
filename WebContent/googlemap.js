6641d442901a0915b4744dc21ec0f5199ffebc6e
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-09-04 13:52:10 -0500

    1
    1

--- WebContent/googlemap.js
+++ WebContent/googlemap.js
@@ -67,29 +67,29 @@ function initAutocomplete() {
     });
 }
 
-
-const findMyState = ()=> {
-    const status = document.querySelector('.status');
-
-    const success = (position) =>{
-        console.log(position)
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-
-        const geoApiUrl = 'http://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}localityLanguage=en'
-
-        fetch(geoApiUrl)
-            .then(res => res.json())
-            .then(data => {
-               status.textContent = data.city
-            })
-    }
-
-    const err = (position) =>{
-       status.textContent = 'Unable to retrieve your location'
-    }
-
-    navigator.geolocation.getCurrentPosition(success, err);
-
-}
-document.querySelector('#find-state').addEventListener('click', findMyState);
+//
+// const findMyState = ()=> {
+//     const status = document.querySelector('.status');
+//
+//     const success = (position) =>{
+//         console.log(position)
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+//
+//         const geoApiUrl = 'http://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}localityLanguage=en'
+//
+//         fetch(geoApiUrl)
+//             .then(res => res.json())
+//             .then(data => {
+//                status.textContent = data.city
+//             })
+//     }
+//
+//     const err = (position) =>{
+//        status.textContent = 'Unable to retrieve your location'
+//     }
+//
+//     navigator.geolocation.getCurrentPosition(success, err);
+//
+// }
+// document.querySelector('#find-state').addEventListener('click', findMyState);
