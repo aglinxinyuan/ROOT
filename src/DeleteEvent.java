52c0f159cf17e9ca95fc0653382c3c087dcc6d73
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-09-03 22:14:13 -0500

    1
    1

---
+++ src/DeleteEvent.java
@@ -0,0 +1,53 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@WebServlet(name = "DeleteEvent", urlPatterns = "/api/deleteEvent")
+public class DeleteEvent extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/ez-cross");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        response.setStatus(200);
+        User user = (User) request.getSession().getAttribute("user");
+        int id = Integer.parseInt(request.getParameter("id"));
+        try (Connection conn = dataSource.getConnection()) {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("DELETE FROM ezcross.event_user WHERE event_id ="+id+" AND "+ "user_id="+user.GetId()+";");
+            rs.close();
+            statement.close();
+
+            // set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
+}
