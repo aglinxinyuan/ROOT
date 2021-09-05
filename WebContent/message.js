c5a48b36e52ebbccd56c4b027810c254e6a3d403
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-09-05 02:47:55 -0500

    1
    1

--- WebContent/message.js
+++ WebContent/message.js
@@ -8,27 +8,29 @@ function handleChatResult(resultData) {
         let rowHTML;
         rowHTML ="";
         rowHTML +=
-            '<li style="display: none"><div class="friendBox mb-3 pl-3">'+
+            '<li><a type="button" href="chat.html"><div class="chatBox mb-3">'+
             '<div class="row">'+
-            '<div class="col-3 mt-3 pl-2 pr-0"><img id="person" src="img/alex.png" height=65% alt=""></div>'+
+            '<div class="col-3 mt-2 pl-2 pr-0"><span class="person"><img id="person" src="img/alex.png" height=65% alt=""></span></div>'+
             '<div class="col-9 mt-2 pl-0 pr-0">'+
             '<div class="row pt-2">'+
             '<div class="col-8">'+
             '<div id="name" class="semiSC_5 ">'+ resultData[i]["name"]+'</div>'+
             '</div>'+
             '<div class="col-4">'+
-            '<a type="button" class="btn addBtn" href="api/addFriend?id='+resultData[i]["id"]+'">Add</a>' +
+            '<div id="time">time</div>'+
             '</div>'+
             '</div>'+
             '<div class="row mt-1">'+
             '<div class="col ">'+
-            '<div id="emailAddress" class="semiSC_4">'+ resultData[i]["email"]+'</div>'+
+            '<div id="emailAddress" class="semiSC_4">'+ resultData[i]["name"]+'</div>'+
             '</div>'+
             '</div>'+
             '</div>'+
-            '</div></div></li>';
+            '</div></div></a></li>';
         element.append(rowHTML);
     }
+
+
 
 
 
@@ -37,6 +39,6 @@ function handleChatResult(resultData) {
 $.ajax({
     dataType: "json", // Setting return data type
     method: "GET", // Setting request method
-    url: "api/userlist", // Setting request url, which is mapped by StarsServlet in Stars.java
+    url: "api/chatRoom", // Setting request url, which is mapped by StarsServlet in Stars.java
     success: (resultData) => handleChatResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
 });
