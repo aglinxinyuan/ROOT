5f0f9b52a38c1c6995dfb7095c31ae08c9da40f0
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-09-06 10:41:32 -0500

    1
    1

--- WebContent/activity.js
+++ WebContent/activity.js
@@ -5,28 +5,38 @@ function handleMainResult(resultData) {
     const d = new Date();
     const date = d.getFullYear() + String(d.getMonth() + 1).padStart(2, '0') + String(d.getDate()).padStart(2, '0');
 
+    let activityDate =  resultData['date'].substring(0,4) + resultData['date'].substring(5,7)+ resultData['date'].substring(8,10);
+
+    console.log(date);
+    console.log(activityDate);
+
     $("#activityTitle").text(resultData['title'])
     $("#name").text(resultData['creator'])
     $("#level").text(resultData['skill'])
     $("#time").text(resultData['date'])
     $("#location").text(resultData['location'])
     $("#about").text(resultData['description'])
+
+    if (date>activityDate){
+        btn.addClass('disabled')
+        btn.text("Finished")
+    }
+    else{
+
         if (resultData['joined']) {
             btn.addClass('disabled')
             btn.text("Joined")
         }
+
         else{
-            if (date>resultData["date"]){
-                btn.addClass('disabled')
-                btn.text("Finished")
-            }
-            else{
-                btn.html('<a href="api/join'+window.location.search+'">Join</a>')
-                btn.click(function() {
-                    window.location.replace('activity.html'+window.location.search);
-                });
-            }
+            btn.html('<a href="api/join'+window.location.search+'">Join</a>')
+            btn.click(function() {
+                window.location.replace('activity.html'+window.location.search);
+            });
+
         }
+
+    }
 }
 
 $.ajax({
