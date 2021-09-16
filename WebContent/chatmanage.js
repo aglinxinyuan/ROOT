bacafc97cd4e7896102fdd978bea28d4db8a8352
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-09-16 01:23:20 -0400

    1
    1

--- WebContent/chatmanage.js
+++ WebContent/chatmanage.js
@@ -1,17 +1,17 @@
 
 
-function deleteChat(id){
-   console.log("deletechat here");
-
-    $.ajax({
-        dataType: "json", // Setting return data type
-        method: "GET", // Setting request method
-        url: "api/deleteChat", // Setting request url, which is mapped by StarsServlet in Stars.java
-        success: (resultData) => handleCalendarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
-    });
-
-}
-
+// function deleteChat(id){
+//    console.log("deletechat here");
+//
+//     $.ajax({
+//         dataType: "json", // Setting return data type
+//         method: "GET", // Setting request method
+//         url: "api/deleteChat", // Setting request url, which is mapped by StarsServlet in Stars.java
+//         success: (resultData) => handleCalendarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+//     });
+//
+// }
+//
 
 
 
---
+++ src/DeleteChatRoom.java
@@ -0,0 +1,51 @@
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+@WebServlet(name = "DeleteChatRoom", urlPatterns = "/api/deleteChatRoom")
+public class DeleteChatRoom extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/ez-cross");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        response.setStatus(200);
+        User user = (User) request.getSession().getAttribute("user");
+        int id = Integer.parseInt(request.getParameter("id"));
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "DELETE FROM ezcross.group_user WHERE id ="+id+" AND user_id="+user.GetId()+ ";";
+            PreparedStatement statement = conn.prepareStatement(query);
+            System.out.println(statement);
+            statement.executeUpdate();
+            statement.close();
+            PrintWriter fresh = response.getWriter();
+            fresh.write("<html><head><meta http-equiv=\"refresh\" content=\"0; url='../message.html'\" /></head></html>");
+            response.setStatus(200);
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
+}
