c8911c422d64f65abbadc577e99fb3e54f73c512
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2021-09-16 20:34:04 -0400

    1
    1

--- WebContent/chatmanage.js
+++ WebContent/chatmanage.js
@@ -1,18 +1,43 @@
 
+function handleCalendarResult(resultData) {
 
-// function deleteChat(id){
-//    console.log("deletechat here");
+        console.log(resultData);
+        let id=parseInt(window.location.search.substring(4,1));
+        console.log(id);
+
+        let rowHTML;
+            let element = $("#eventsjoined");
+            rowHTML = '<li> <div class="eventDetial row">';
+            rowHTML +=
+                '<button class="button" onclick="openModal('+id+')">'+
+                    '<div class="row">'+
+                        '<div class="col-12 semiSC_6"><a data-toggle="modal" data-target="#ModalDelete">Delete</a>'+
+                        '</div>'+
+                    '</div>'+
+                '</button>'
+            element.append(rowHTML);
+
+}
+
+function openModal(id) {
+    $('#ModalDelete').modal('show')
+        $('#delete').html('<a type="button" class="btn" href="api/deleteChatRoom?id='+id+'">Delete</a>')
+
+}
+
+$.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/eventjoined", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleCalendarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
+
+
 //
-//     $.ajax({
-//         dataType: "json", // Setting return data type
-//         method: "GET", // Setting request method
-//         url: "api/deleteChat", // Setting request url, which is mapped by StarsServlet in Stars.java
-//         success: (resultData) => handleCalendarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
-//     });
-//
+// function activityInfo(id){
+//     window.location.replace('activity.html?id='+id);
 // }
 //
-
-
+//
 
 
